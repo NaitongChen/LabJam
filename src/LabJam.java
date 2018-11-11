a23f3aed83e584fd4c4ca5b40137204f0bc73096
NaitChen <35083192+NaitChen@users.noreply.github.com>
2018-11-11 12:40:21 -0800

    Initial Upload

---
+++ src/LabJam.java
@@ -0,0 +1,67 @@
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import panels.Tab1;
+import panels.Tab2;
+
+public class LabJam {
+	JFrame frame = new JFrame("LabJAM");
+	JPanel mainPanel = new JPanel();
+	JPanel panel2 = new JPanel();
+	JButton button2 = new JButton("switch to 1");
+	CardLayout cl = new CardLayout();
+	Connection con;
+	
+	public LabJam() {
+		connect("ora_q7l0b", "a21539151");
+		mainPanel.setLayout(cl);
+		Tab1 tab1 = new Tab1(cl, mainPanel, con);
+		Tab2 tab2 = new Tab2(cl, mainPanel, con);
+
+		mainPanel.add(tab1, "1");
+		mainPanel.add(tab2, "2");
+		
+		frame.add(mainPanel);
+		frame.setPreferredSize(new Dimension(400, 400));
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new LabJam();
+			}
+		});
+	}
+	
+	private void connect(String username, String password) {
+		
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		} catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
+			System.exit(-1);
+		}
+		
+		String connectURL = "jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug"; 
+		
+		try {
+			con = DriverManager.getConnection(connectURL,username,password);
+			con.setAutoCommit(false);
+			System.out.println("\nConnected to Oracle!");
+		} catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
+	    }
+    }
+}
---
+++ src/panels/Tab1.java
@@ -0,0 +1,73 @@
+package panels;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.proteanit.sql.DbUtils;
+
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+
+public class Tab1 extends JPanel {
+	private JTable table;
+
+	/**
+	 * Create the panel.
+	 */
+	public Tab1(CardLayout cl, JPanel mainPanel, Connection con) {
+		setLayout(null);
+		
+		JButton switchButton = new JButton("Switch to 2");
+		switchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cl.show(mainPanel, "2");
+			}
+		});
+		switchButton.setBounds(25, 34, 115, 29);
+		add(switchButton);
+		
+		JButton refreshButton = new JButton("Refresh");
+		refreshButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectAll(con);
+			}
+		});
+		refreshButton.setBounds(204, 34, 115, 29);
+		add(refreshButton);
+		
+		table = new JTable();
+		table.setBounds(44, 100, 349, 164);
+		selectAll(con);
+		add(table);
+	}
+	
+	private void selectAll(Connection con) {
+		int     nid;
+		String     name;
+		String     major;
+		Statement  stmt;
+		ResultSet  rs = null;
+		   
+		try{
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM tab1");
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			// close the statement; 
+			// the ResultSet will also be closed
+			stmt.close();
+			
+		} catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
+		}
+	}
+}
