babf1ea94ad6d2f4eb8d98f5c1c23e48315acabd
NaitChen <35083192+NaitChen@users.noreply.github.com>
2018-11-14 12:43:14 -0800

    Create researcher.sql

---
+++ scripts/researcher.sql
@@ -0,0 +1,53 @@
+-- For researchers:
+--     We take researcher id when they login
+--     We show the projects that they are working on, lab they are in, their PI, etc.
+--     Show tables of all labs, all lab members, all lab managers, all researchers, all pi's.
+--     When they click on a lab, we enter lab profile panel.
+--     This is where we display all lab specific info (pi, researchers, projects, etc.)
+--     Clicking on a project will take user to the project info panel described in the collaborator sql file.
+
+-- getting all lab names and fields
+Select *
+from Lab;
+
+-- getting all information of all lab members
+Select *
+from Contains_LabMember;
+
+-- getting all information of all lab members from a particular lab
+Select *
+from Contains_LabMember
+where labId = labId_input;
+
+-- getting all information of all lab managers
+Select *
+from Contains_LabMember cl, LabManager lm
+where lm.id = cl.id;
+
+-- select lab manager for a particular lab
+Select *
+from Contains_LabMember cl, LabManager lm
+where lm.id = cl.id and
+    cl.labId = labId_input;
+
+-- select all PI
+Select *
+from Contains_LabMember cl, PI pi
+where pi.id = cl.id;
+
+-- select PI of one lab
+Select *
+from Contains_LabMember cl, PI pi
+where pi.id = cl.id and
+    cl.labId = labId_input;
+
+-- select all researchers
+Select *
+from Contains_LabMember cl, Researcher r
+where r.id = cl.id;
+
+-- select all researchers of one lab
+Select *
+from Contains_LabMember cl, Researcher r
+where r.id = cl.id and
+    cl.labId = labId_input;
