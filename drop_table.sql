a7389c2e9cc38fb91725c8f7e54b014f26531e91
NaitChen <35083192+NaitChen@users.noreply.github.com>
2018-11-14 12:03:51 -0800

    Add files via upload

---
+++ drop_table.sql
@@ -0,0 +1,35 @@
+DROP TABLE Makes_Booking;
+
+DROP TABLE Participates;
+
+DROP TABLE Takes_Booking;
+
+DROP TABLE Subject;
+
+DROP TABLE Name_Education_ProjectName;
+
+DROP TABLE Assigned_Collaborators_Id;
+
+DROP TABLE Role_WeeklyHoursAllocated;
+
+DROP TABLE Supervises_WorksOn;
+
+DROP TABLE Fund_ApprovedGrant;
+
+DROP TABLE Date_Name_Lmid;
+
+DROP TABLE Applies_OpenGrant_Date;
+
+DROP TABLE MaterialType_MaterialPrice;
+
+DROP TABLE Project_MaterialType;
+
+DROP TABLE Researcher;
+
+DROP TABLE PI;
+
+DROP TABLE LabManager;
+
+DROP TABLE Contains_LabMember;
+
+DROP TABLE Lab;
