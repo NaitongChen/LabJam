69d3b04d61e0b045c3bcf04fbb9e07007674c319
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-14 17:11:50 -0800

    Create labmanager.sql

---
+++ scripts/labmanager.sql
@@ -0,0 +1,9 @@
+-- For lab managers:
+--     We take lab manager id when they login
+--     We show all projects in their lab, lab they are in, their PI, etc.
+--     Show tables of all labs, all lab members, all lab managers, all researchers, all pi's.
+--     When they click on a lab, we enter lab profile panel.
+--     This is where we display all lab specific info (pi, researchers, projects, etc.)
+--     Clicking on a project will take user to the project info panel described in the collaborator sql file.
+
+--     All queries from researcher.sql are available to lab manager too
