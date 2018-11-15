e747215a99d7f384960a04fd232d2931ca608695
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-14 18:44:22 -0800

    added 3/5 claimed queries from google doc

--- scripts/dama_queries.sql
+++ scripts/dama_queries.sql
@@ -1,16 +1,23 @@
 -- untested fancy queries claimed from the google doc
 
--- query list of available Subjects with requested startTime and Booking length: ONLY LAB MANAGERS
-
-
+-- query list of available Subjects with requested dateParticipated, startTime and Booking length: ONLY LAB MANAGERS
+select *
+from Subjects s
+where availability = 'Y' AND s.id NOT IN (select p.id
+                                         from Participates p, Takes_Booking b
+                                         where p.dateParticipated = dateParticipated_input AND startTime_input <= p.startTime <= (startTime_input + b.length));
+                                         
 -- get how much funding is left in a project after materials are purchased (assuming we only purchase once, at the start of the project)
-
+(TOTAL_GRANT_MONEY - select m.materialPrice
+                    from Project_MaterialType pm, MaterialType_MaterialPrice m
+                    where pm.projectName = projectName_input AND pm.materialType = m.materialType;)
 
 -- get number of participants in each condition for a project
+select participantTestCondition, COUNT(*)
+from Takes_Booking
+group by participantTestCondition;
 
+-- query the expertise of researchers in each project
 
+                                          
 -- query the number of researchers with a specific expertise given a project
-
-
--- query total number of weekly hours allocated by a given researcher 
-
