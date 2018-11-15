95494bc06e78ef8c041af4b5db88574e462e6727
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-14 20:48:10 -0800

    5/5 fancy queries done
    Marked with a green D in the google doc

--- scripts/dama_queries.sql
+++ scripts/dama_queries.sql
@@ -5,19 +5,26 @@ select *
 from Subjects s
 where availability = 'Y' AND s.id NOT IN (select p.id
                                          from Participates p, Takes_Booking b
-                                         where p.dateParticipated = dateParticipated_input AND startTime_input <= p.startTime <= (startTime_input + b.length));
+                                         where b.projectName = projectName_input AND p.dateParticipated = dateParticipated_input AND (startTime_input <= p.startTime <= (startTime_input + b.length))
+                                         );
                                          
 -- get how much funding is left in a project after materials are purchased (assuming we only purchase once, at the start of the project)
 (TOTAL_GRANT_MONEY - select m.materialPrice
                     from Project_MaterialType pm, MaterialType_MaterialPrice m
-                    where pm.projectName = projectName_input AND pm.materialType = m.materialType;)
+                    where pm.projectName = projectName_input AND pm.materialType = m.materialType);
 
 -- get number of participants in each condition for a project
 select participantTestCondition, COUNT(*)
 from Takes_Booking
 group by participantTestCondition;
 
--- query the expertise of researchers in each project
-
+-- query the expertise/researcherType of researchers in a given project
+select researcherType
+from Researcher r, Supervises_WorksOn w
+where r.id = w.rid, w.projectName = projectName_input;
                                           
 -- query the number of researchers with a specific expertise given a project
+select researcherType, SUM(*)
+from Researcher r, Supervises_WorksOn w
+group by researcherType
+having r.id = w.rid, w.projectName = projectName_input;
