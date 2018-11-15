ced6db40647d1686ce5fc29b7b06736536e1f00b
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-14 18:03:44 -0800

    Added basic Lab Manager queries

--- scripts/labmanager.sql
+++ scripts/labmanager.sql
@@ -7,3 +7,71 @@
 --     Clicking on a project will take user to the project info panel described in the collaborator sql file.
 
 --     All queries from researcher.sql are available to lab manager too
+
+-- see all open grants
+select *
+from Applies_OpenGrant_Date;
+
+-- see open grants that have been approved (but still open)
+select * 
+from Applies_OpenGrant_Date
+where status = "approved";
+
+-- see open grants that have been declined
+select * 
+from Applies_OpenGrant_Date
+where status = "declined";
+
+-- see open grants that have been applied to
+select * 
+from Applies_OpenGrant_Date
+where status = "applied";
+
+-- see all grants for a given project
+select * 
+from Fund_ApprovedGrant
+where projectName = projectName_input
+
+-- insert subjects
+insert into Subjects (id, name, availability)
+values (id_input, name_input, availability_input);
+
+-- update subject name
+update Subjects
+set name = name_input
+where id = id_input;
+
+-- update subject availability
+update Subjects
+set availability - availability_input
+where id = id_input;
+
+-- insert collaborators ** come back to this
+insert into Assigned_Collaborators (id, name, education)
+values (id_input, name_input, education_input);
+
+-- insert bookings (need a project associated w/ them) - whenever a booking is inserted, we ALSO need to run a separate query to add to makes_booking
+insert into Takes_Booking (projectName, participantNumber, length, participantTestCondition)
+values (projectName_input, participantNumber_input, length_input, participantTestCondition_input);
+
+-- insert into makes_booking - projName and partNumber should come from the query above ^^ lmid should be lmid of Lab Manager that ran the query above ^^
+insert into Makes_Booking (projectName, participantNumber, lmid)
+values (projectName_input, participantNumber_input, lmid)
+
+-- update booking - only length and participantTestCondition are updateable
+update Takes_Booking
+set length = length_input, participantTestCondition = participantTestCondition_input
+where projectName = projectName_input and participantNumber = participantNumber_input;
+
+-- update open grant status - should only be changed to applied, approved, declined, not applied
+update Applies_OpenGrant_Date
+set status = status_input
+where name = name_input and amount = amount_input;
+
+-- see all subjects
+select * 
+from Subjects;
+
+-- see all subjects participating in projects
+select *
+from Participates;
