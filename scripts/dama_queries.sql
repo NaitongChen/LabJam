0e5934854174baa74540602f0b79a70e7d7d7a57
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-14 18:12:10 -0800

    Create dama_queries.sql

---
+++ scripts/dama_queries.sql
@@ -0,0 +1,16 @@
+-- untested fancy queries claimed from the google doc
+
+-- query list of available Subjects with requested startTime and Booking length: ONLY LAB MANAGERS
+
+
+-- get how much funding is left in a project after materials are purchased (assuming we only purchase once, at the start of the project)
+
+
+-- get number of participants in each condition for a project
+
+
+-- query the number of researchers with a specific expertise given a project
+
+
+-- query total number of weekly hours allocated by a given researcher 
+
