e2a194841380196b721aa60580df0f920a1f2272
dcorrech <36525352+dcorrech@users.noreply.github.com>
2018-11-16 08:30:01 -0800

    Added comments cross-referencing with dama_queries.sql

--- scripts/christina_fancy_queries_all.sql
+++ scripts/christina_fancy_queries_all.sql
@@ -4,6 +4,7 @@
 SELECT SID FROM Participates WHERE ProjectName = projectName_input;
 
 -- Query list of available Subjects with requested startTime and Booking length 
+-- DAMA: this doesn't take into account the availability of Subjects (s.availability = "Y"), and the length in this query is to check for conflicts - please see my version of this in dama_queries.sql
 SELECT s.id, s.name
 FROM Subjects s, Participates p, Takes_Booking t
 WHERE s.id = p.sid AND startTime = startTime_input AND t.length = length_input;
@@ -38,7 +39,7 @@ Group by projectName;
 
 
 -- Get how much funding is left in a project after materials are purchased (assuming we only purchase once, at the start of the project)
-
+-- DAMA: MUCH better version that my query for this - just need to change "WITH" to "CREATE VIEW" no? - this takes care of the fact that we need to create views too
 With TOTAL_GRANT_MONEY as 
 (Select SUM(amount), projectName 
 From Fund_ApprovedGrant f
@@ -54,6 +55,7 @@ SELECT TOTAL_GRANT_MONEY - DOLLAR_SPENT;
 
 
 -- Get number of participants in each condition for a project
+-- DAMA: do we need to group by projectName if we are providing input? There will only be one group, so i think we just need to group by participantTestCondition only?
 select participantTestCondition, projectName, COUNT(*)
 from Takes_Booking
 where projectName = projectName_input
@@ -61,12 +63,14 @@ group by participantTestCondition, projectName;
 
 
 -- Query the expertise of researchers in a givenproject - I think by experise we meant researcherType
+-- DAMA: same as in dama_queries.sql
 select researcherType
 from Researcher r, Supervises_WorksOn w
 where r.id = w.rid and w.projectName = projectName_input;
 
 
 -- Query the number of researchers with each expertise/researchType given a project 
+-- DAMA: wording of this query was a bit ambiguous - your version takes both a projectName and researcherType_input and gives us the total, mine takes only projectName and returns the count for EACH researcherType (with a group by researcherType). Both are correct, might as well keep them both?
 select count(*)
 from Researcher r, Supervises_WorksOn w
 where r.id = w.rid and w.projectName = projectName_input and researcherType = researcherType_input;
